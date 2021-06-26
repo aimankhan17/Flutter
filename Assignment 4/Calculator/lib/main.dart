f9037d12a21f7ead62e082710ba6cad0513a592c
aimankhan17 <68400836+aimankhan17@users.noreply.github.com>
2021-06-26 19:42:24 +0500

    Assignment 4 (TODO, Calculator, E-com Navigation)

---
+++ Assignment 4/Calculator/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'Calculator.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Calculator'),
+          ),
+          body: Calculator()),
+    );
+  }
+}
