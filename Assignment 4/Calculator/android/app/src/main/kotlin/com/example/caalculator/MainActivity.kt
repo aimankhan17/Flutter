f3a4ba52f809c6259a1b835206731d90fb942403
aimankhan17 <68400836+aimankhan17@users.noreply.github.com>
2021-07-07 23:21:50 +0500

    commit...

---
+++ Assignment 4/Calculator/android/app/src/main/kotlin/com/example/caalculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.caalculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ E-Commerce Navigations/lib/home.dart
@@ -0,0 +1,86 @@
+import 'dart:ui';
+
+import 'package:ecom/screen1.dart';
+import 'package:ecom/screen3.dart';
+import 'package:ecom/screen4.dart';
+import 'package:flutter/material.dart';
+
+import 'screen2.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int currentTab = 0;
+
+  Widget currentScreen = Screen1();
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(  floatingActionButton: new FloatingActionButton(
+    child: const Icon(Icons.search),onPressed: (){},
+    backgroundColor: Colors.purple,
+  ),
+  floatingActionButtonLocation:    
+      FloatingActionButtonLocation.miniCenterDocked,
+  
+      body: PageStorage(bucket: PageStorageBucket(), child: currentScreen),
+      bottomNavigationBar: BottomAppBar(
+        // notchMargin: 50,
+        color: Colors.white,
+        child: Container(
+          height: 60,
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                MaterialButton(
+                    minWidth: 50,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Screen1();
+                        currentTab = 0;
+                      });
+                    },
+                    child: Icon(Icons.home,
+                        color: currentTab == 0 ? Colors.purple : Colors.grey)),
+                MaterialButton(
+                    minWidth: 60,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Screen2();
+                        currentTab = 1;
+                      });
+                    },
+                    child: Icon(Icons.favorite,
+                        color: currentTab == 1 ? Colors.purple : Colors.grey)),
+                MaterialButton(
+                    minWidth: 60,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Screen3();
+                        currentTab = 2;
+                      });
+                    },
+                    child: Icon(Icons.card_travel,
+                        color: currentTab == 2 ? Colors.purple : Colors.grey)),
+                MaterialButton(
+                    minWidth: 60,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Screen4();
+                        currentTab = 3;
+                      });
+                    },
+                    child: Icon(Icons.person,
+                        color: currentTab == 3 ? Colors.purple : Colors.grey)),
+                
+              ]),
+        ),
+      ),
+    );
+  }
+}
